8ad2c83762c04fb7291796924a68e703c35fe808
legeannd <gean.lfpsa@gmail.com>
2019-01-30 10:39:09 -0300

    Questão do balanceamento

---
+++ ED/Estruturas de manipulação de dados/balanceamento.py
@@ -0,0 +1,21 @@
+from pilhas import Pilha
+exp = '3-[15+2*(4-3)*[2+(5-1)]]/4'
+p = Pilha(len(exp))
+bem = True
+for i in range(len(exp)):
+    if exp[i] in '{[(':
+        p.empilhar(exp[i])
+    elif exp[i] in ')' and p.desempilhar() != '(':
+        bem = False
+        break
+    elif exp[i] in '}' and p.desempilhar() != '{':
+        bem = False
+        break
+    elif exp[i] in ']' and p.desempilhar() != '[':
+        bem = False
+        break
+
+bem = p.vazia()
+print('Expressão bem formada') if bem == True else print('Expressão mal formada')
+print(bem)
+p.imprimir()
