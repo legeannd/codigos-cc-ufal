6a22a4d7ad645bc8e6ae637224121a01094c5c13
Gean Lucas <gean.lfpsa@gmail.com>
2019-02-14 22:02:11 -0300

    Adição da fila encadeada

---
+++ ED/Estruturas de manipulação de dados/Fila/fila_encadeada.py
@@ -0,0 +1,38 @@
+class Node:
+    item = None
+    next = None
+
+    def __init__(self, item):
+        self.item = item
+
+class chainedQueue:
+    start = None
+    end = None
+    size = 0
+
+    def __init__(self):
+        self.start = Node(None)
+        self.end = self.start
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def push(self,item):
+        self.end.next = Node(item)
+        self.end = self.end.next
+        self.size += 1
+
+    def pop(self):
+        if not self.isEmpty():
+            aux = self.start.next
+            self.start.next = aux.next
+            item = aux.item
+            self.size -= 1
+            return item
+
+    def queuePrint(self):
+        aux = self.start.next
+        while aux != None:
+            print(aux.item)
+            aux = aux.next
+        print('---')
