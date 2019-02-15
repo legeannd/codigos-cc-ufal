af08c9b1505b8ffd0912715500c8926e522030b1
Gean Lucas <gean.lfpsa@gmail.com>
2019-02-15 00:01:28 -0300

    Questão da ordenação quase funcionando

---
+++ ED/Estruturas de manipulação de dados/Fila/ordenacao_fila.py
@@ -0,0 +1,33 @@
+from fila_encadeada import chainedQueue
+
+pacotes = [1,2,3,5,6,8,10,4,7,9,15,12,11,13,14]
+fila = chainedQueue()
+aux = chainedQueue()
+
+for i in pacotes:
+    if fila.isEmpty():
+        fila.push(i)
+    else:
+        print('o i é: ',i)
+        if fila.end.item > i:
+            print('ENTROU NO I ', i)
+            while fila.start.next.item < i:
+                popinho = fila.pop()
+                aux.push(popinho)
+
+                print('enfileirou',popinho)
+            aux.push(i)
+            print('enfileirou fora do while', i)
+            print('principal')
+            fila.queuePrint()
+            print('auxiliar')
+            aux.queuePrint()
+
+        else:
+            fila.push(i)
+while not fila.isEmpty():
+    aux.push(fila.pop())
+print('principal')
+fila.queuePrint()
+print('auxiliar')
+aux.queuePrint()
